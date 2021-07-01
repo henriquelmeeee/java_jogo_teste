0b2bca60b96f9f767bd86e2767569eea1f2ca456
henriquelmeeee <86432400+henriquelmeeee@users.noreply.github.com>
2021-07-01 18:13:52 -0300

    Create Inimigo.java

---
+++ Inimigo.java
@@ -0,0 +1,15 @@
+package classes;
+
+public class Inimigo {
+	
+	public int vida = 100;
+	public String inimigotipo = "tipo";
+	public int dano = 10;
+
+	
+	public int getvida() {
+		
+		vida = 100;
+		return vida;
+	}
+}
