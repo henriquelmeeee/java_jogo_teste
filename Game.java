aa5009928da1599c8f0d712ba2370eebffe3a49d
henriquelmeeee <86432400+henriquelmeeee@users.noreply.github.com>
2021-07-01 18:12:59 -0300

    Create Game.java

---
+++ Game.java
@@ -0,0 +1,30 @@
+package classes;
+
+public class Game {
+	
+	public static void main(String[] args) {
+		
+		jogador player = new jogador();
+		Inimigo inimigo = new Inimigo();
+		ElfInimigo elf = new ElfInimigo();
+		
+		if(player.nome == "Guilhermee") {
+		
+		player.perdervida();
+		player.perdervida();
+		
+		System.out.println(player.vida);
+		}else {
+			
+			
+			player.vida = player.vida - elf.dano;
+			
+		}
+		
+		player.ganharvida();
+		
+		System.out.println(player.vida);
+		
+	}
+
+}
